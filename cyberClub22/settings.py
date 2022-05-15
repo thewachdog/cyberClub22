632f9d8e71bb030d0049a61a1b26faf6a5c26037
Bharath V <sit20it059@sairamtap.edu.in>
2022-05-16 03:36:29 +0530

    Deploying to Heroku

--- cyberClub22/settings.py
+++ cyberClub22/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
