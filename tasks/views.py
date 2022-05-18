03cec80f68f1ca1073a0c4911b2948836b7a0748
Bharath V <sit20it059@sairamtap.edu.in>
2022-05-19 05:29:06 +0530

    Fixed 404 redirection error

--- tasks/views.py
+++ tasks/views.py
@@ -7,9 +7,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def handler404(request):
-    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+    return render(request, '404.html', {})
 
 # Create your views here.
 def index(request):
